bf9d8d878613e1c91fd638eae6dc514a746b2dd7
Davis Willard <dwillard@er-a5.kzn.21-school.ru>
2021-08-18 16:24:45 +0300

    second commit

--- sources/args.c
+++ sources/args.c
@@ -12,7 +12,7 @@
 
 #include "../push_swap.h"
 
-static void	freedom(char ***ret)
+/*static void	freedom(char ***ret)
 {
 	int	count;
 
@@ -24,14 +24,35 @@ static void	freedom(char ***ret)
 	}
 	free(*ret);
 	*ret = NULL;
-}
+}*/
 
 static void	put_ind(int **integer, int **index, char **argv)
 {
 	int	min;
-	int ind;
+	int	ind;
+	int	min_old;
+	int	count;
+	int	indd;
 
-	index = 0;
+	indd = 0;
+	count = 0;
+	min_old = 0x80000000;
+	while (indd != count - 1)
+	{
+		count = 0;
+		min = ft_atoi(argv[count + 1]);
+		while (argv[++count])
+		{
+			if (min >= ft_atoi(argv[count]) && ft_atoi(argv[count]) >= min_old)
+			{
+				min = ft_atoi(argv[count]);
+				ind = count - 1;
+			}
+		}
+		min_old = min;
+		(*integer)[ind] = min;
+		(*index)[ind] = indd++;
+	}
 }
 
 static int	**args(char **argv)
@@ -58,20 +79,20 @@ static int	**args(char **argv)
 	return (ret);
 }
 
+/*	char	**split;
+
+ if (argc == 2)
+{
+	split = ft_split(argv[1], ' ');
+	arr = args(split);
+	freedom(&split);
+}*/
 t_stack	*get_args(int argc, char **argv)
 {
 	t_stack	*ret;
 	int		**arr;
-	char	**split;
 
-	if (argc == 2)
-	{
-		split = ft_split(argv[1], ' ');
-		arr = args(split);
-		freedom(&split);
-	}
-	else
-		arr = args(argv);
-	ret = ft_lstnew('a','b');
+	arr = args(argv);
+	ret = ft_lstnew(**arr, **(arr + 1));
 	return (ret);
 }
